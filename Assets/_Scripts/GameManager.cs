d9635e3cc88fb4e76398a580333efd9f9e7b6e98
Bennet <bennet@metricminds.com>
2023-04-17 00:55:53 +0200

    Finalize board initialization

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,14 +13,24 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private Node nodePrefab;
+    
+    [SerializeField]
+    private SpriteRenderer boardPrefab;
 
     public void Start()
     {
-        var cam = Helper.Camera;
+        var centerPos = new Vector3((boardWidth/2)-0.5f, (boardHeight/2)-0.5f,0f);
+
+        var parent = new GameObject("GameBoard");
         
+        var cam = Helper.Camera;
         cam.transform.position = new Vector3((boardWidth/2)-0.5f, (boardHeight/2)-0.5f,-10f);
         cam.orthographicSize = (boardHeight / 2)+0.5f;
+
+        var board = Instantiate(boardPrefab, centerPos, quaternion.identity, parent.transform);
+        board.size = new Vector2(boardWidth, boardHeight);
+        
         grid = new Grid();
-        grid.Init(boardWidth, boardHeight, nodePrefab, transform);
+        grid.Init(boardWidth, boardHeight, nodePrefab, parent.transform);
     }
 }
