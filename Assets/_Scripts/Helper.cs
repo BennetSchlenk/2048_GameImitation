704c8286b492082630c76d676e619869d405903d
Bennet <bennet@metricminds.com>
2023-04-16 22:24:35 +0200

    Add Helper class

---
+++ Assets/_Scripts/Helper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class Helper
+{
+    private static Camera camera;
+
+    public static Camera Camera
+    {
+        get
+        {
+            if(camera == null)camera = Camera.main;
+            return camera;
+        }
+    }
+
+}
