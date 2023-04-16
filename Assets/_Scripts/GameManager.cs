c73e62656e3d8d0eac98503c72bd0b3230bf5292
Bennet <bennet@metricminds.com>
2023-04-16 21:14:34 +0200

    Add Gamemanager

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Grid grid;
+
+    [SerializeField]
+    private int boardWidth;
+
+    [SerializeField]
+    private int boardHeight;
+
+    [SerializeField]
+    private Node nodePrefab;
+
+    public void Start()
+    {
+        grid = new Grid();
+        grid.Init(boardWidth, boardHeight, nodePrefab, transform);
+    }
+}
