ea297c7197dee799730fe8b7072278441f2a06bb
Bennet <bennet@metricminds.com>
2023-05-07 01:06:08 +0200

    Add set block method and change block colors to pastel color

--- Assets/_Scripts/Block.cs
+++ Assets/_Scripts/Block.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Block : MonoBehaviour
 {
+    
     public uint Value;
 
     [SerializeField]
@@ -13,12 +15,22 @@ public class Block : MonoBehaviour
 
     [SerializeField]
     private TextMeshPro text;
+    
+    public Vector2 Position => transform.position;
+    public Node Node;
 
     public void Init(BlockType type)
     {
         Value = type.Value;
-        renderer.color = type.color;
+        renderer.color = type.Color;
         text.text = type.Value.ToString();
+    }
+
+    public void SetBlock(Node newNode)
+    {
+        if (Node != null) Node.occupyingBlock = null;
+        newNode.occupyingBlock = this;
+        Node = newNode;
     }
 }
 
@@ -26,5 +38,5 @@ public class Block : MonoBehaviour
 public struct BlockType
 {
     public uint Value;
-    public Color color;
+    public Color Color;
 }
